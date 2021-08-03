57d965766adeb4518619b04b4310f9868ae8060b
Eva María de León Fajardo <evamariadeleon@gmail.com>
2021-08-03 11:25:38 -0600

    ejercicio 2019
    - Data hasta 2019
    - Parámetro EMI 2019 (ParamTotalCPILegacyRebase)
    - muestreo: ResampleScrambleVarMonths

--- scripts/InflationPercentile/Eval-InflationPercentile.jl
+++ scripts/InflationPercentile/Eval-InflationPercentile.jl
@@ -13,8 +13,8 @@ addprocs(4, exeflags="--project")
 @everywhere using HEMI
 
 # CountryStructure con datos hasta diciembre de 2020
-gtdata_eval = gtdata[Date(2020, 12)]
-
+# gtdata_eval = gtdata[Date(2020, 12)]
+gtdata_eval = gtdata[Date(2019, 12)]
 
 ##
 # Percentiles ponderados
@@ -26,7 +26,8 @@ gtdata_eval = gtdata[Date(2020, 12)]
 # utilizamos los mismos datos gtdata_eval = gtdata[Date(2020, 12)]
 
 # Funciones de remuestreo y tendencia
-resamplefn = ResampleSBB(36)
+# resamplefn = ResampleSBB(36)
+resamplefn = ResampleScrambleVarMonths()
 trendfn = TrendRandomWalk()
 
 # la función de inflación, la instanciamos dentro del diccionario.
@@ -34,15 +35,15 @@ dict_percW = Dict(
     :inflfn => InflationPercentileWeighted.(50:80), 
     :resamplefn => resamplefn,
     :trendfn => trendfn,
-    :nsim => 125_000) |> dict_list 
+    :nsim => 125_000) |> dict_list # 125_000
 
 
 # 2. Definimos las carpetas para almacenar los resultados 
-savepath_pw = datadir("InflationPercentileWeighted")
-savepath_plot_pw = plotsdir("InflationPercentileWeighted","MSE")
+savepath_pw = datadir("InflationPercentileWeighted","scramble")
+savepath_plot_pw = plotsdir("InflationPercentileWeighted","scramble","MSE")
 
 # 3. Usamos run_batch, para gnenerar la evaluación de los percentiles del 50 al 80
-run_batch(gtdata_eval, dict_percW, savepath_pw)
+run_batch(gtdata_eval, dict_percW, savepath_pw, param_constructor_fn=ParamTotalCPILegacyRebase, rndseed = 0)
 
 # 4. Revisión de resultados, usando collect_results
 df_pw = collect_results(savepath_pw)
@@ -62,9 +63,9 @@ CSV.write(string(savepath_pw,"//InflationPercentileWeighted.csv"),sorted_df_pw)
 scatter(50:80, df_pw.mse, 
     ylims = (0, 15),
     label = " MSE Percentiles Ponderados", 
-    legend = :topleft, 
+    legend = :topright, 
     xlabel= "Percentil ponderado", ylabel = "MSE")
-    annotate!(60, 12, string("min MSE = ",min_mse_pw,"  ",min_pw),7)
+    annotate!(70, 12, string("min MSE = ",min_mse_pw,"  ",min_pw),7)
     png(savepath_plot_pw)
 
 
@@ -78,7 +79,8 @@ scatter(50:80, df_pw.mse,
 # utilizamos los mismos datos gtdata_eval = gtdata[Date(2020, 12)]
 
 # Funciones de remuestreo y tendencia
-resamplefn = ResampleSBB(36)
+# resamplefn = ResampleSBB(36)
+resamplefn = ResampleScrambleVarMonths()
 trendfn = TrendRandomWalk()
 
 # la función de inflación, la instanciamos dentro del diccionario.
@@ -90,11 +92,11 @@ dict_perc = Dict(
 
 
 # 2. Definimos las carpetas para almacenar los resultados 
-savepath_p = datadir("InflationPercentileEq")
-savepath_plot_p = plotsdir("InflationPercentileEq","MSE")
+savepath_p = datadir("InflationPercentileEq","scramble")
+savepath_plot_p = plotsdir("InflationPercentileEq","scramble","MSE")
 
 # 3. Usamos run_batch, para gnenerar la evaluación de los percentiles del 50 al 80
-run_batch(gtdata_eval, dict_perc, savepath_p)
+run_batch(gtdata_eval, dict_perc, savepath_p, param_constructor_fn=ParamTotalCPILegacyRebase, rndseed = 0)
 
 # 4. Revisión de resultados, usando collect_results
 df_p = collect_results(savepath_p)
@@ -114,9 +116,9 @@ CSV.write(string(savepath_p,"//InflationPercentileEq.csv"),sorted_df_p)
 scatter(50:80, df_p.mse, 
     ylims = (0, 20),
     label = " MSE Percentiles Equiponderados", 
-    legend = :topleft, 
+    legend = :topright, 
     xlabel= "Percentil Equiponderado", ylabel = "MSE")
-    annotate!(60, 16, string("min MSE = ",min_mse_p,"  ",min_p),7)
+    annotate!(70, 16, string("min MSE = ",min_mse_p,"  ",min_p),7)
     png(savepath_plot_p)
 
 
